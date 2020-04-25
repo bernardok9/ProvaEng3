7735aca948be1ae76899916eef72cebe323b5ebe
Bernardo Kopp <m1t0.fearless@gmail.com>
2020-04-25 01:05:34 -0300

    Falta Love e corrigir problemas

--- src/app.js
+++ src/app.js
@@ -5,38 +5,42 @@ import express from 'express';
 import cors from 'cors';
 import { uuid } from 'uuidv4';
 
-const app = express();
 
+
+const app = express();
 app.use(express.json());
 app.use(cors());
+
 
 let products = []; //id, code, description, buyPrice, sellPrice, tags:[], lovers;
 
 app.get('/products', (request, response) => { //Okay
   // TODO: listagem de todos os produtos
-  return response.json(products);
+
+  response.json(products);
 });
 
-app.post('/products', (request, response) => { //Okay
+app.post('/products', (request, response) => { //FAZER O TESTE DO LOVERS PRA SABER SE PODE ZERAR OU ALTERAR COM OS EXISTENTES
   // TODO: Salvar produto no array products
   const product = request.body;
-  products[0].id = uuid(); 
-  products.push(product)
-  response.json(product);
+  product.id = uuid();
+  products.filter(num => num.code == product.code);
+  products.push(product);
+
+  response.status(201).json(product);
 
 });
 
-app.put('/products/:id', (request, response) => { //Okay Maybe;; Look Tags
+app.put('/products/:id', (request, response) => { //AJEITAR SE NÃO EXISTIR
   // TODO: Atualiza produto por ID
   const id = request.params.id;
-  const code = request.body.code; 
-  const description = request.body.description; 
-  const buyPrice = request.body.buyPrice; 
-  const sellPrice = request.body.sellPrice; 
+  const code = request.body.code;
+  const description = request.body.description;
+  const buyPrice = request.body.buyPrice;
+  const sellPrice = request.body.sellPrice;
   const tags = request.body.tags;
 
   let product = products.filter(num => num.id = id);
-
   product[0].code = code;
   product[0].description = description;
   product[0].buyPrice = buyPrice;
@@ -44,27 +48,44 @@ app.put('/products/:id', (request, response) => { //Okay Maybe;; Look Tags
   product[0].tags = tags;
 
 
-  response.json(product[0]);
+
+  response.json(product);
 });
+
 
 app.delete('/products/:code', (request, response) => {
   // TODO: Remove TODOS os produtos que possuam o código passado por parâmetro
-  const code = request.params.code;
-  products = products.filter(num => num != code);
-  response.json(products);
+  const {code} = request.params;
+  const product = products.findIndex(num => num.code == code);
+  if(product == -1){
+    response.status(400).send();
+  }else{
+    products.splice(product,1);
+    response.status(204).send();
+  }
+  
+
 });
+
 
 app.post('/products/:code/love', (request, response) => {
   // TODO: Incrementa em 1 o número de lovers de todos os produtos que possuam 
   // o code do parâmetro
-  const code = request.params.code;
-  
+  const product = products.map(num => num.code == request.params.code ? num.lovers++: num.lovers)
+  response.json(products)
 });
 
-app.get('/products/:code', (request, response) => { //Okay
+app.get('/products/:code', (request, response) => { //okay
   // TODO: Busca de todos os produtos com o código recebido por parâmetro.
-  const product = products.filter(num => num.code == request.params.code);
+  const {code} = request.params;
+  const product = products.find(num => num.code == code);
+ 
+  if(product == undefined){
+    response.status(204)
+  }  
+
   response.json(product);
+
 });
 
 export default app;
